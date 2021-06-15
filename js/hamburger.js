dec25e7015fb9f18a34be7607a29e0d839263b23
jfredyromero <66575625+jfredyromero@users.noreply.github.com>
2021-06-15 18:58:23 -0500

    Menu lateral añadido

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,7 @@
-
+// Para desplegar menu lateral
+const sidePanel = document.querySelector('.side-panel');
 const nav = document.querySelector('#hamburger');
 nav.addEventListener('click', e =>{
     nav.classList.toggle('open');
+    sidePanel.classList.toggle('open');
 })
