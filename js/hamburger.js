70eba621136693cd750eb5c7008dc8fbf9543f0e
jfredyromero <66575625+jfredyromero@users.noreply.github.com>
2021-06-15 22:02:47 -0500

    Añadido la funcion para clickear fuera del side menu y ocultarlo. Añadido el despliegue de sublistas

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,26 @@
 // Para desplegar menu lateral
 const sidePanel = document.querySelector('.side-panel');
+const emptyPanel = document.querySelector('.empty-panel');
 const nav = document.querySelector('#hamburger');
-nav.addEventListener('click', e =>{
+nav.addEventListener('click', () =>{
     nav.classList.toggle('open');
     sidePanel.classList.toggle('open');
+    emptyPanel.classList.toggle('open');
+})
+
+emptyPanel.addEventListener('click',()=>{
+    nav.classList.toggle('open');
+    sidePanel.classList.toggle('open');
+    emptyPanel.classList.toggle('open');
+})
+
+
+
+// Para desplegar subopciones
+document.querySelectorAll('.list-item').forEach(e=>{
+    e.addEventListener('click', event=>{
+        event.target.querySelectorAll('.sublist-item').forEach(o=>{
+            o.classList.toggle('open');
+        })
+    })
 })
