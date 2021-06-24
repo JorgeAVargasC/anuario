008f9859a46e73e9b4d5969c4277acf6a390e978
jfredyromero <66575625+jfredyromero@users.noreply.github.com>
2021-06-24 12:56:24 -0500

    Movimiento lateral automatico añadido

--- js/main.js
+++ js/main.js
@@ -1,27 +1,52 @@
-// Para desplegar menu lateral
-const sidePanel = document.querySelector('#side-menu');
-const emptyPanel = document.querySelector('#empty-panel');
-const hamburger = document.querySelector('#hamburger');
+document.addEventListener('DOMContentLoaded', ()=>{
+    // Para desplegar menu lateral
+    const sidePanel = document.querySelector('#side-menu');
+    const emptyPanel = document.querySelector('#empty-panel');
+    const hamburger = document.querySelector('#hamburger');
 
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('open');
-    sidePanel.classList.toggle('open');
-    emptyPanel.classList.toggle('open');
-})
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
+        sidePanel.classList.toggle('open');
+        emptyPanel.classList.toggle('open');
+    })
 
-emptyPanel.addEventListener('click', () => {
-    hamburger.classList.toggle('open');
-    sidePanel.classList.toggle('open');
-    emptyPanel.classList.toggle('open');
-})
+    emptyPanel.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
+        sidePanel.classList.toggle('open');
+        emptyPanel.classList.toggle('open');
+    })
 
 
-// Para desplegar subopciones
-document.querySelectorAll('.list-item').forEach(e => {
-    e.addEventListener('click', event => {
-        event.target.querySelectorAll('.sublist-item').forEach(o => {
-            o.classList.toggle('open');
+    // Para desplegar subopciones
+    document.querySelectorAll('.list-item').forEach(e => {
+        e.addEventListener('click', event => {
+            event.target.querySelectorAll('.sublist-item').forEach(o => {
+                o.classList.toggle('open');
+            })
         })
     })
+
+    // Para movimiento lateral del timeline
+    function repeatWhileMouseOver(element, action, time) {
+        var interval = null;
+        element.addEventListener('mouseover', function() {
+            interval = setInterval(action, time);
+        });
+    
+        element.addEventListener('mouseout', function() {
+            clearInterval(interval);
+        });
+    }
+
+    const leftArrow = document.querySelector('#left-arrow');
+    repeatWhileMouseOver(leftArrow, ()=>{
+        document.querySelector('#timeline').scrollLeft -= 2;
+    }, 10);
+
+    const rightArrow = document.querySelector('#right-arrow');
+    repeatWhileMouseOver(rightArrow, ()=>{
+        document.querySelector('#timeline').scrollLeft += 2;
+    }, 10);   
+    
 })
