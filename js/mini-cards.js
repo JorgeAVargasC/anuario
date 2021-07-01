b56a2bd106543e4d683357482698ae47fdd3ddc8
jfredyromero <66575625+jfredyromero@users.noreply.github.com>
2021-07-01 11:09:12 -0500

    Vista del mosaico agregada

--- js/mini-cards.js
+++ js/mini-cards.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', ()=>{ 
+document.addEventListener('DOMContentLoaded', () => {
+    let miniCards = document.querySelectorAll('.glass-hover');
+    miniCards.forEach((obj) => {
+        obj.addEventListener('click', (event) => {
+            let cardView = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector('#card-view');
+            // cardView.style.display = 'block';
+        })
+    });
+
     let flipContainer = document.querySelector('.flip-container');
     flipContainer.addEventListener('click', function() {
         flipContainer.classList.toggle('is-flipped');
