0b9caac1ed605e1ba921894c198f9eaa6412cecb
jfredyromero <66575625+jfredyromero@users.noreply.github.com>
2021-07-10 19:08:22 -0500

    php actualizado

--- php/consultarUsuarios.php
+++ php/consultarUsuarios.php
@@ -1,11 +1,11 @@
 <?php
     include 'conexion.php';
     $conection = mysqli_connect($host, $user, $pw, $db);
-    $query = 'SELECT * from miembros'
+    $query = "SELECT * from miembros";
     $result = mysqli_query($conection, $query);
     $jsonArray = array();
     while($row = mysqli_fetch_assoc($result)){
         $jsonArray[] = $row;
     }
-    print_r($jsonArray)
+    print_r(json_encode($jsonArray))
 ?>
