bc763471236f1e3f0ba4594bef75fe81b31c2604
JorgeVargas <javargas216@unicauca.edu.co>
2021-09-07 13:04:51 -0500

    Funciona compresion de img, falta integrar
    Co-authored-by: jfredyromero

--- admin/modelo-prueba.php
+++ admin/modelo-prueba.php
@@ -5,59 +5,114 @@ include("../functions/comprimir_imagenes.php");
 
 if ($_POST['registro'] == 'nuevo') {
 
-  try {
+	try {
 
-    include_once "../connection/db_connection.php";
-    $conn = mysqli_connect($host, $user, $pw, $db);
+		include_once "../connection/db_connection.php";
+		$conn = mysqli_connect($host, $user, $pw, $db);
 
-    $primerNombre = $_POST["primerNombre"];
-    $primerApellido = $_POST["primerApellido"];
-    echo "Primer Nombre: ".$primerNombre."<br>";
-    echo "Primer Apellido: ".$primerApellido."<br>";
+		$primerNombre = $_POST["primerNombre"];
+		$primerApellido = $_POST["primerApellido"];
+		echo "Primer Nombre: " . $primerNombre . "<br>";
+		echo "Primer Apellido: " . $primerApellido . "<br>";
 
-    if ($_FILES['imagen-miembro']['name'] != null ) {
-      $fileSize = $_FILES['imagen-miembro']['size'];
-      $fileExtensionsAllowed = ['jpeg', 'jpg', 'png'];
-      $path = $_FILES['imagen-miembro']['name'];
-      $ext = strtolower(pathinfo($path, PATHINFO_EXTENSION));
-      $validacion3 = true;
+		if ($_FILES['imagen-miembro']['name'] != null) {
+			$fileSize = $_FILES['imagen-miembro']['size'];
+			$fileExtensionsAllowed = ['jpeg', 'jpg', 'png'];
+			$path = $_FILES['imagen-miembro']['name'];
+			$ext = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+			$validacion3 = true;
 
-      if (!in_array($ext, $fileExtensionsAllowed)) {
-        $validacion3 = false;
-      }      
-    } 
+			if (!in_array($ext, $fileExtensionsAllowed)) {
+				$validacion3 = false;
+			}
+		}
 
-    date_default_timezone_set('America/Bogota');
-    $primerNombre_img = strtolower(quitar_tildes($primerNombre));
-    $primerApellido_img = strtolower(quitar_tildes($primerApellido));
-    $urlFoto = 'foto_' . $primerNombre_img . '_' . $primerApellido_img . '_' . date('Ymd_his') . '.' . $ext;
+		date_default_timezone_set('America/Bogota');
+		$primerNombre_img = strtolower(quitar_tildes($primerNombre));
+		$primerApellido_img = strtolower(quitar_tildes($primerApellido));
+		$urlFoto = 'foto_' . $primerNombre_img . '_' . $primerApellido_img . '_' . date('Ymd_his') . '.' . $ext;
 
-    //subida foto
-    $directorio = "../img/members/";
-    if (!is_dir($directorio)) {
-      mkdir($directorio, 0755, true);
-    }
+		//subida foto
+		$directorio = "../img/members/";
+		if (!is_dir($directorio)) {
+			mkdir($directorio, 0755, true);
+		}
 
-    if (move_uploaded_file($_FILES['imagen-miembro']['tmp_name'], $directorio . $urlFoto)) {
-      $imagen_url = $directorio . $urlFoto;  //it should change when every image would be on the same path
-      echo "Url img: ".$imagen_url."<br>";
-      echo "Se subió correctamente";
-    } else {      
-      echo ("nada");
-    }      
+		// PRUEBA COMPRIMIR IMAGENES
 
-  } catch (Exception $e) {
-    //throw $th;
-    $respuesta = array(
-      'respuesta' => $e->getMessage()
-    );
-  }
+		// function compressImage($source, $destination, $quality)
+		// {
+		//   // Obtenemos la información de la imagen
+		//   $imgInfo = getimagesize($source);
+		//   $mime = $imgInfo['mime'];
+
+		//   // Creamos una imagen
+		//   switch ($mime) {
+		//     case 'image/jpeg':
+		//       $image = imagecreatefromjpeg($source);
+		//       break;
+		//     case 'image/png':
+		//       $image = imagecreatefrompng($source);
+		//       break;
+		//     case 'image/gif':
+		//       $image = imagecreatefromgif($source);
+		//       break;
+		//     default:
+		//       $image = imagecreatefromjpeg($source);
+		//   }
+
+		//   // Guardamos la imagen
+		//   imagejpeg($image, $destination, $quality);
+
+		//   // Devolvemos la imagen comprimida
+		//   return $destination;
+		// }
+
+		$source = $_FILES['imagen-miembro']['tmp_name'];
+
+		// Obtenemos la información de la imagen
+		$imgInfo = getimagesize($source);
+		$mime = $imgInfo['mime'];
+		$imgsize = $fileSize / 1024;
+		$reduction = $imgsize / 50;
+		$quality = 100 - $reduction;
+
+
+		//$quality = 10;
+
+		echo("FILESIZE: ".$fileSize. "Bytes");
+		echo("<br>");
+		echo("El tamaño original es de ". $imgsize." KB");
+		echo("<br>");
+		echo("Se reduce al :  " . $quality . " %");
+		echo("<br>");
+
+		// Creamos una imagen
+		switch ($mime) {
+			case 'image/jpeg':
+				$image = imagecreatefromjpeg($source);
+				break;
+			case 'image/png':
+				$image = imagecreatefrompng($source);
+				break;
+			case 'image/gif':
+				$image = imagecreatefromgif($source);
+				break;
+			default:
+				$image = imagecreatefromjpeg($source);
+		}
+
+		if (imagejpeg($image, $directorio . $urlFoto, $quality)) {
+			$imagen_url = $directorio . $urlFoto;  //it should change when every image would be on the same path
+			echo "Url img: " . $imagen_url . "<br>";
+			echo "Se subió correctamente";
+		} else {
+			echo ("NADA QUE FUNCIONA PERRA");
+		}
+	} catch (Exception $e) {
+		//throw $th;
+		$respuesta = array(
+			'respuesta' => $e->getMessage()
+		);
+	}
 }
-
-?>
-
-
-
-
-
-
--- templates/datos-miembro-prueba.php
+++ templates/datos-miembro-prueba.php
@@ -1,82 +1,80 @@
 <div class="card-body"></div>
 <div class="form-group">
-		<input type="text" class="form-control" id="primerNombre" name="primerNombre" placeholder="Ingresa tu Primer Nombre" required>
-		<label for="primerNombre">Primer Nombre</label>
-	</div>
-	<div class="form-group">
-		<input type="text" class="form-control" id="segundoNombre" name="segundoNombre" placeholder="Ingresa tu Segundo Nombre">
-		<label for="segundoNombre">Segundo Nombre</label>
-	</div>
-  <div class="form-group">
-		<input type="text" class="form-control" id="primerApellido" name="primerApellido" placeholder="Ingresa tu Primer Apellido" required>
-		<label for="primerApellido">Primer Apellido</label>
-	</div>
+	<input type="text" class="form-control" id="primerNombre" name="primerNombre" placeholder="Ingresa tu Primer Nombre" required>
+	<label for="primerNombre">Primer Nombre</label>
+</div>
+<div class="form-group">
+	<input type="text" class="form-control" id="segundoNombre" name="segundoNombre" placeholder="Ingresa tu Segundo Nombre">
+	<label for="segundoNombre">Segundo Nombre</label>
+</div>
+<div class="form-group">
+	<input type="text" class="form-control" id="primerApellido" name="primerApellido" placeholder="Ingresa tu Primer Apellido" required>
+	<label for="primerApellido">Primer Apellido</label>
+</div>
 
-
-
-	<div class="form-group">
-		<div class="input-group">
-			<input type="file" id="imagen-miembro" name="imagen-miembro" accept="image/png, image/jpeg, image/jpg" required>
-			<label for="imagen-miembro">Imagen</label>
-		</div>
+<div class="form-group">
+	<div class="input-group">
+		<input type="file" id="imagen-miembro" name="imagen-miembro" accept="image/png, image/jpeg, image/jpg" required>
+		<label for="imagen-miembro">Imagen</label>
 	</div>
-	<div class="form-group">
-		<div id="cropped-image-container">
-			<img id="cropped-image" src="#"S alt="Cropped Image">
-		</div>	
+</div>
+<div class="form-group">
+	<div id="cropped-image-container">
+		<img id="cropped-image" src="#" S alt="Cropped Image">
 	</div>
-	<!-- Modal -->
-	<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h4 class="modal-title" id="modalLabel">Personaliza tu imagen</h4>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					<div class="image-preview">
-						<div id="image-container">
-							<img id="image" src="#" alt="Picture">
-						</div>
-						<div id="result">
-							<h5>Previsualización:</h5>
-							<br>
-							<div id="image-result">
-							</div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="modalLabel">Personaliza tu imagen</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="image-preview">
+					<div id="image-container">
+						<img id="image" src="#" alt="Picture">
+					</div>
+					<div id="result">
+						<h5>Previsualización:</h5>
+						<br>
+						<div id="image-result">
 						</div>
 					</div>
 				</div>
-				<div class="modal-footer">
-					<button type="button" id="crop-button" class="btn btn-primary">Crop</button>
-					<button type="button" class="btn btn-danger" data-dismiss="modal">Exit</button>
-				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" id="crop-button" class="btn btn-primary">Crop</button>
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Exit</button>
 			</div>
 		</div>
 	</div>
+</div>
 
 
 
-	<!-- Check boxes -->
-	
+<!-- Check boxes -->
 
-	<br>
-	<h4>Cargos que ocupó</h4>
-	<?php
-	foreach ($array_cargos as $cargo) {
-		if ($cargo['id'] != 10) {
-			// Coordinador TET se convierte a coordinadorTET
-			$cargo_format = lcfirst(str_replace(' ', '', quitar_tildes($cargo['cargo'])));
-	?>
-			<div class="form-check">
-				<input type="checkbox" class="form-check-input checkbox-cargos" id="<?php echo $cargo_format; ?>" name="<?php echo $cargo_format; ?>" value="<?php echo $cargo_format; ?>">
-				<label for="<?php echo $cargo_format; ?>" class="form-check-label"><?php echo $cargo['cargo'] ?></label>
-			</div>
-	<?php
-		}
+
+<br>
+<h4>Cargos que ocupó</h4>
+<?php
+foreach ($array_cargos as $cargo) {
+	if ($cargo['id'] != 10) {
+		// Coordinador TET se convierte a coordinadorTET
+		$cargo_format = lcfirst(str_replace(' ', '', quitar_tildes($cargo['cargo'])));
+?>
+		<div class="form-check">
+			<input type="checkbox" class="form-check-input checkbox-cargos" id="<?php echo $cargo_format; ?>" name="<?php echo $cargo_format; ?>" value="<?php echo $cargo_format; ?>">
+			<label for="<?php echo $cargo_format; ?>" class="form-check-label"><?php echo $cargo['cargo'] ?></label>
+		</div>
+<?php
 	}
-	?>
+}
+?>
 
 
 </div>
